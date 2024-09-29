f5f46866646d888d85cdfb0ab83a6214d7a72e72
nithin <nithaart2406@gmail.com>
2024-09-29 11:21:02 +0530

    added assignment

--- matgeo_questions/1-1.9-1/codes/plotting.py
+++ matgeo_questions/1-1.9-1/codes/plotting.py
@@ -18,17 +18,13 @@ def customize_plot():
     ax.spines['bottom'].set_position('zero')#repositions the bottom axis at y=0
 
     # Uncomment the following lines if you want to add labels to the axes
-    # plt.xlabel('$x$')
-    # plt.ylabel('$y$')
+    #plt.xlabel('$x$')
+    #plt.ylabel('$y$')
     
     plt.legend(loc='best')#adds legend to the plot and loc='best' automatically chooses the location that minimizes overlap with the actual plot elements
     plt.grid(True)#shows the grid
     plt.axis('equal')#ensures each unit on the x-axis is equal to that on y-axis
 
-# Example usage:
-# After plotting your data, call this function to apply the customizations:
-# customize_plot()
-# plt.show()
 
 def plot_points(points, labels):
     """
@@ -50,28 +46,21 @@ def plot_points(points, labels):
                      ha='center') # Horizontal alignment can be left, right, or center
 
 
-data = np.genfromtxt('values.dat', delimiter=' ', names=True)
-x = data['x']
-y = data['y']
-A = np.array(([x[0], y[0]])).reshape(-1,1) 
-B = np.array(([x[1], y[1]])).reshape(-1,1)
-# Example usage:
-# A = np.array([1, 2])
-# B = np.array([4, 6])
-# C = np.array([8, 3])
-# points = np.block([[A, B, C]])
-# labels = ['A', 'B', 'C']
-# plot_points_with_labels(points, labels)
-
 
 def plot_line(P, Q):
     x_PQ = line_gen(P,Q)#GVV function
 
     # Plot the line
     plt.plot(x_PQ[0, :], x_PQ[1, :], label=f'Line from ({P[0]}, {P[1]}) to ({Q[0]}, {Q[1]})')#plots the x and y coordinates in X_AB
+
 data = np.genfromtxt('values.dat', delimiter=' ', names=True)
 x = data['x']
 y = data['y']
 P = np.array(([x[0], y[0]])).reshape(-1,1)
 Q = np.array(([x[1], y[1]])).reshape(-1,1)
 plot_line(P, Q)
+points = np.block([[P, Q]])
+labels = ['A', 'B']
+plot_points(points, labels)
+customize_plot()
+plt.show()
